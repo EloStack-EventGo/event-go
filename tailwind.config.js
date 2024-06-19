278f721800dcf018eae36406982de7ffc0d4a3eb
chandolusravya <chandolusravya123@gmail.com>
2024-06-20 00:42:33 +0530

    login updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
     extend: {
       colors: {
         offwhite: '#D9D9D9',
+        black:"#313131",
+        yellow:"#fdd201",
       },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
@@ -18,6 +20,7 @@ module.exports = {
       backgroundImage: {
         logo: "url('../public/logo.png')",
         concert: "url('../public/concert.jpg')",
+        crowd: "url('../public/background.webp')",
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
